824595dee7e73042c766810546eaacca302b9bbf
Germán Aliprandi <galiprandi@gmail.com>
2021-01-16 03:18:49 -0300

    Formateo de núemros

--- main.js
+++ main.js
@@ -1,69 +1,73 @@
-console.log("La calculadora de Santi");
+console.log("La calculadora de Santi")
 
-const buttons = Array.from(document.querySelectorAll(".btn"));
-const display = document.getElementById("lcd");
+const buttons = Array.from(document.querySelectorAll(".btn"))
+const display = document.getElementById("lcd")
+const formatter = new Intl.NumberFormat("en") // Formater number
 
 // detecta cuando se apreta un botón
 buttons.forEach((btn) => {
   btn.addEventListener(
     "mouseup",
     (e) => {
-      keyPress(e.target.innerText);
-      e.preventDefault();
+      keyPress(e.target.innerText)
+      e.preventDefault()
     },
     false
-  );
+  )
   btn.addEventListener(
     "touchend",
     (e) => {
-      keyPress(e.target.innerText);
-      e.preventDefault();
+      keyPress(e.target.innerText)
+      e.preventDefault()
     },
     false
-  );
-});
+  )
+})
 
 // realiza una acción cuando se apreta un botón
 function keyPress(value) {
-  console.log(value);
-
   if (value === "▸") {
-    display.innerText = display.innerText.slice(0, -1);
-    return;
+    display.innerText = display.innerText.slice(0, -1)
+    return
   }
   if (value === "±") {
-    let lcd = display.innerText;
-    if (lcd.slice(0, 1) === "-") lcd = lcd.slice(1);
-    else lcd = "-" + lcd;
-    display.innerText = lcd;
-    return;
+    let lcd = display.innerText
+    if (lcd.slice(0, 1) === "-") lcd = lcd.slice(1)
+    else lcd = "-" + lcd
+    display.innerText = lcd
+    return
   }
 
-  if (value === "·") value = ".";
-  if (value === "AC" || value === "C") value = display.innerText = "";
+  if (value === "·") value = "."
+  if (value === "AC" || value === "C") value = display.innerText = ""
   if (value === "=") {
-    let lcd = display.innerText;
-    lcd = lcd.replace(/×/g, "*").replace(/÷/g, "/");
+    let lcd = display.innerText
+    // Reemplazo símbolo de divición
+    lcd = lcd.replace(/×/g, "*").replace(/÷/g, "/")
+    // Reemplazo raíz cuadrada
     if (lcd.indexOf("√") > -1) {
-      lcd = lcd.replace("√", "Math.sqrt(") + ")";
+      lcd = lcd.replace("√", "Math.sqrt(") + ")"
     }
-    console.log(lcd);
-    lcd = parseFloat(eval(lcd)); //.toFixed(3);
-    display.innerText = lcd;
-    return;
+
+    // Evalua la operación
+    lcd = eval(lcd)
+    lcd = formatter.format(lcd)
+
+    display.innerText = lcd
+    return
   }
-  show = display.innerHTML + value;
+  show = display.innerHTML + value
   // show = show.replace("¡hola!", "");
-  display.innerText = show;
-  display.scrollLeft = 1000000;
+  display.innerText = show
+  display.scrollLeft = 1000000
 }
 
 // Detectar teclas
 document.onkeydown = function (e) {
-  e = e || window.event;
-  let value = e.key;
-  if (value === "Enter") keyPress("=");
-  if (value === "*") keyPress("×");
-  if (value === "/") keyPress("÷");
-  if (value < 10 || value === "+" || value === "-") keyPress(value);
-};
+  e = e || window.event
+  let value = e.key
+  if (value === "Enter") keyPress("=")
+  if (value === "*") keyPress("×")
+  if (value === "/") keyPress("÷")
+  if (value < 10 || value === "+" || value === "-") keyPress(value)
+}
